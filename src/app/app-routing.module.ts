1cabd6b81658d6ccfdd03f0a24573a88a1fc266e
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2019-03-06 13:09:18 -0500

    pages and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'forgot', loadChildren: './forgot/forgot.module#ForgotPageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
 ];
 
 @NgModule({
